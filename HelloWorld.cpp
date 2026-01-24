2c0b43caf2ad3439800ab480fdd548c4920cd8ac
krodriguez804 <krodriguez804@gmail.com>
2026-01-24 10:40:15 -0800

    Finished part 1

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,1 +1,12 @@
 // Complete the code
+// Krystle Dao 
+// CS 20 First Assignment
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
